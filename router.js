f2b0ff7b68d82a122813ea11b64b57442a24e9cf
Jonathan Dorety <kyullog@gmail.com>
2019-03-02 13:11:24 -0500

    post endpoint is implemented and responds with newly created post

--- router.js
+++ router.js
@@ -4,6 +4,33 @@ const db = require("./data/db.js");
 
 const router = express.Router();
 
+router.post("/", (req, res) => {
+  const post = req.body;
+  if (!post.title || !post.contents) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide title and contents for the post" });
+  } else {
+    db.insert(post)
+      .then(response => {
+        db.findById(response.id)
+          .then(response => {
+            res.status(201).json(response);
+          })
+          .catch(err =>
+            res.status(500).json({
+              error: "There was an error retriving your newly created post"
+            })
+          );
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  }
+});
+
 router.get("/", (req, res) => {
   db.find()
     .then(notes => {
