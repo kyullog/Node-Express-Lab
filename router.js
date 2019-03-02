be5f33ce97cbed8f0638c6551f01e034cdbdee52
Jonathan Dorety <kyullog@gmail.com>
2019-03-02 12:55:02 -0500

    findById endpoint is implemented and responds correctly

--- router.js
+++ router.js
@@ -5,8 +5,36 @@ const db = require("./data/db.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log("The router is working");
-  res.status(200).json("The response is working");
+  db.find()
+    .then(notes => {
+      console.log(notes);
+      res.status(200).json(notes);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const noteID = req.params.id;
+  db.findById(noteID)
+    .then(note => {
+      if (note.length) {
+        res.status(200).json(note);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved" });
+    });
 });
 
 module.exports = router;
