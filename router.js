34fe46718148583714ea28fb059e1ff70f5a543b
Jonathan Dorety <kyullog@gmail.com>
2019-03-02 12:39:46 -0500

    router.js is connected and resonding

--- router.js
+++ router.js
@@ -4,4 +4,9 @@ const db = require("./data/db.js");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  console.log("The router is working");
+  res.status(200).json("The response is working");
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const server = express();
 const router = require("./router.js");
 
 server.use(express.json());
-
-server.get("/", (req, res) => {
-  console.log("1, 2, 1, 2 this is just a test");
-  res.status(200).json("This is a test");
-});
+server.use("/api/posts", router);
 
 module.exports = server;
